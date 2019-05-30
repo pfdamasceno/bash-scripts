981e2f5ffe4a655b831b71ae099318e623a4904e
Pablo Damasceno <pablodamasceno@gmail.com>
2019-05-30 09:08:48 -0700

    add working version of storage watchdog to monitor disk space

--- storage-watchdog/storage_watchdog.sh
+++ storage-watchdog/storage_watchdog.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-LOCATION=/media/rajlab/sachin_data_1
+LOCATION=/
 THRESHOLD=75
-USAGE=$(df $location| grep $location | awk '{ print $5}' | sed 's/%//g')
+USAGE=$(df $LOCATION| grep $LOCATION | awk '{ print $5}' | sed 's/%//g')
 
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-    mail -s 'Disk Space Alert' mailid@domainname.com << EOF
+if [ "$USAGE" -gt "$THRESHOLD" ] ; then
+    mail -s 'Disk Space Alert' pablo.damasceno@ucsf.edu << EOF
 Your root partition remaining free space is critically low. Used: $CURRENT%
 EOF
 fi
 
-# To run it daily, save the script to storage_watchdog.sh in your home directory,
+# To run it daily, save the script to the file sample.sh in your home directory,
 # change the email to your email,
-# and add the following line at the end of /etc/crontab file:
-# @daily ~/sample.sh
-# by Mallik Arjun
+# and add the following line at the end of /etc/crontab file
